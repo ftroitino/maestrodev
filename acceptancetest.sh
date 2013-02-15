736ebab178811dd36e9c2f1d55d276b856bb05da
fgt223 <fgt223@fisherman.site>
2013-02-15 14:06:22 +0100

    add acceptancetest.sh

---
+++ acceptancetest.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+cd /opt/pdi/owd/push_server/test/functions/
+resultado=$(node E2E.js)
+if [ "$resultado" = "" ]; then
+ echo "Todo correcto"
+else
+  echo "Error: $resultado"
+  exit 1
+fi
